216eb07e57b0117825bbdc6dde7caa7a5b75dbb1
AldaPro777 <1aaldahirmolinamedina@gmail.com>
2024-10-06 13:34:10 -0600

    afsd

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const routesUsers = require("./routes/users.routes.js");
+const routesDashboard = require("./routes/dashboard.routes.js");
+const routesStudyResources = require("./routes/studyResources.routes.js");
+
+const connectDB = require("./config/db.js");
+
+const app = express();
+
+//* Middlewares
+app.use(cors());
+app.use(express.json());
+
+//* Rutas
+app.use("/api/users", routesUsers);
+app.use("/api/dashboard", routesDashboard);
+app.use("/api/studyResources", routesStudyResources);
+
+//* Conexión a la base de datos
+connectDB();
+
+const PORT = process.env.PORT || 7777;
+
+app.listen(PORT, () => {
+  console.log(`Server Running On PORT ${PORT}`);
+});
