cef542825e24756e0c19b01638c7aaa8f4065477
AldaPro777 <1aaldahirmolinamedina@gmail.com>
2024-10-10 14:51:18 -0600

    dfgh

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const connectDB = require("./config/db.js");
 const app = express();
 
 //* Middlewares
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 //* Rutas
@@ -24,7 +24,7 @@ app.use("/api/studyResources", routesStudyResources);
 //* Conexión a la base de datos
 connectDB();
 
-const PORT =  7516;
+const PORT = 7516;
 
 app.listen(PORT, () => {
   console.log(`Server Running On PORT ${PORT}`);
